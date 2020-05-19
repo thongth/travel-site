b7ed1c893a51dbefa78970e82a4be7262f7ae71c
Sornram Saetang <waterfishX@outlook.com>
2020-05-19 10:52:25 +0700

    Added code splitting

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,17 +1,29 @@
-import '../styles/styles.css';
+import '../styles/styles.css'
+import 'lazysizes'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
-import Modal from './modules/Modal'
 
-new Modal()
+new StickyHeader()
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
+new MobileMenu();
+let modal
 
-let stickyHeader = new StickyHeader();
+document.querySelectorAll(".open-modal").forEach(el => {
+  el.addEventListener("click", e => {
+    e.preventDefault()
+    if (typeof modal == "undefined") {
+      import(/* webpackChunkName: "modal" */ './modules/Modal').then(x => {
+        modal = new x.default()
+        setTimeout(() => modal.openTheModal(), 20)
+      }).catch(() => console.log("There was a problem."))
+    } else {
+      modal.openTheModal()
+    }
+  })
+})
 
-new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
-
-new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
-
-if(module.hot) {
-    module.hot.accept();
+if (module.hot) {
+  module.hot.accept()
 }
